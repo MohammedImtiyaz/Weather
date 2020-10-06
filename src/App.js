80e607981c8b6f913e3af2798e69283b4bb15547
MohammedImtiyaz <mohammed.imtiyaz@signify.com>
2020-10-06 19:13:47 +0530

    MI :  works app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const api = {
+  key: "a586bf71c066de3173f37b3360f6a80b",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      console.log("entered key");
+      fetch(`${api.base}weather?q=${query}&units=metrics&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log("result:" + result);
+        })
+        .catch((e) => {
+          console.log("ther is a error" + e);
+        });
+    }
+  };
+
+  const dateBuilder = function () {
+    return new Date().toISOString();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <main>
+          <div className="search-box">
+            <input
+              type="text"
+              className="search-text"
+              placeholder="Enter the city name..."
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={search}
+            />
+          </div>
+          {(typeof weather.main !=='undefined') ?  (<div><div className="location-box">
+            <div className="location-name">{weather.name} , {weather.sys.country}</div>
+            <div className="location-date">{dateBuilder()}</div>
+          </div>
+          <div className="weather-box">
+            <div className="weather-temp">{Math.round(weather.main.temp)} O F</div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div></div> ) : ('')}
+         
+        </main>
+      </div>
     </div>
   );
 }
